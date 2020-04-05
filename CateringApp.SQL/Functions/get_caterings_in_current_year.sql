99b5035f4477293c55e91725ec441ce10d594f9e
Borna Šarkezi <sarkezi.borna@gmail.com>
2020-04-06 00:53:01 +0200

    Prikaz grafa za broj cateringa po mjesecima u trenutnoj godini

---
+++ CateringApp.SQL/Functions/get_caterings_in_current_year.sql
@@ -0,0 +1,22 @@
+﻿-- https://stackoverflow.com/questions/11479918/include-missing-months-in-group-by-query
+-- https://stackoverflow.com/questions/13437362/how-to-get-the-first-and-last-date-of-the-current-year
+
+CREATE FUNCTION [cat_app].[get_caterings_in_current_year]()
+RETURNS TABLE
+AS
+RETURN 
+
+	with dates(d) as (
+			SELECT DATEADD(MONTH, n, DATEADD(MONTH, DATEDIFF(MONTH, 0, DATEADD(yy, DATEDIFF(yy, 0, GETDATE()), 0)), 0))
+			FROM ( SELECT TOP (DATEDIFF(MONTH, DATEADD(yy, DATEDIFF(yy, 0, GETDATE()), 0), DATEADD(yy, DATEDIFF(yy, 0, GETDATE()) + 1, -1)) + 1) 
+			n = ROW_NUMBER() OVER (ORDER BY [object_id]) - 1
+			FROM sys.all_objects ORDER BY [object_id] ) AS n
+		)
+		SELECT 
+		  [Month]    = MONTH(dates.d), 
+		  CateringCount = COUNT(c.catering_date) 
+		FROM dates LEFT OUTER JOIN cat_app.caterings AS c
+		  ON c.catering_date >= dates.d
+		  AND c.catering_date < DATEADD(MONTH, 1, dates.d)
+		GROUP BY dates.d;
+	
---
+++ CateringApp.Web/TypeScript/Home/Home.ts
@@ -0,0 +1,59 @@
+﻿module Home {
+    export function initialize() {
+        initData();
+    }
+
+    export async function initData() {
+        $.ajax({
+            url: `/api/home/count/open`,
+            method: "get",
+            success: (res: number) => {
+                document.getElementById("open-count").innerText = res.toString();
+            }
+        });
+
+        $.ajax({
+            url: `/api/home/count/closed`,
+            method: "get",
+            success: (res: number) => {
+                document.getElementById("closed-count").innerText = res.toString();
+            }
+        });
+
+        let canvas = <HTMLCanvasElement>document.getElementById("caterings-month-chart");
+        let ctx = canvas.getContext("2d");
+
+        const getRandomColor = () => {
+            var letters = '0123456789ABCDEF'.split('');
+            var color = '#';
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        }
+
+        await $.ajax({
+            url: "/api/home/count/months",
+            method: "get",
+            success: (res: number[]) => {
+
+                console.log(res);
+
+                const options: Chart.ChartConfiguration = {
+                    type: "bar",
+                    data: {
+                        labels: ["Sij", "Velj", "Ožu", "Tra", "Svi", "Lip", "Srp", "Kol", "Ruj", "Lis", "Stu", "Pro"],
+                        datasets: [{
+                            label: "Br. cateringa u mjesecu",
+                            data: res,
+                            backgroundColor: getRandomColor
+                        }]
+                    }
+                };
+
+                const chart = new Chart(ctx, options);
+            },
+            error: Global.ajaxErrorHandler
+        })       
+    }
+}
